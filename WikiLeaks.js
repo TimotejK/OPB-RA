8a710c0fa41d3323abfd06a474d3ab2275987413
Timotej Knez <timotej.knez@gmail.com>
2023-11-09 22:58:44 +0100

    Update WikiLeaks.js

--- WikiLeaks.js
+++ WikiLeaks.js
@@ -35,12 +35,12 @@ relations = [
             [2, "1972-02-25", "...General Azimi, minister of war, asks...", 1, 2],
             [3, "1972-03-09", "...Trials/executions of anti-government...", 2, 3],
             [4, "2001-01-02", "...he also made important contributions...", 3, 1],
-            [5, "2001-01-02", "...and originality have made Einstein s...", 2, 2],
-            [6, "2001-01-02", "...that if the special theory is correc...", 3, 3],
-            [7, "2001-01-02", "...For much of the last phase of his ac...", 1, 1],
-            [8, "2001-01-02", "...he enrolled in the mathematics and p...", 2, 2],
-            [9, "2001-01-02", "...Horrified by the Nazi war of extermi...", 3, 3],
-            [10, "2001-01-02", "...Einstein supported the Allies but ge...", 1, 2]
+            [5, "2000-02-02", "...and originality have made Einstein s...", 2, 2],
+            [6, "2000-02-12", "...that if the special theory is correc...", 3, 3],
+            [7, "2000-01-01", "...For much of the last phase of his ac...", 1, 1],
+            [8, "1999-03-01", "...he enrolled in the mathematics and p...", 2, 2],
+            [9, "2001-05-22", "...Horrified by the Nazi war of extermi...", 3, 3],
+            [10, "2006-01-02", "...Einstein supported the Allies but ge...", 1, 2]
         ]
     }
 ]
